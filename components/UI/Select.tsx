4f8a11399497eb90a043245745f66eccb29382d4
RoyEden <roy.ede@gmail.com>
2022-07-04 19:07:28 -0300

    Updated Select to wrap with a div

--- components/UI/Select.tsx
+++ components/UI/Select.tsx
@@ -1,10 +1,11 @@
 import * as SelectPrimitive from "@radix-ui/react-select";
+import { Slot } from "@radix-ui/react-slot";
 import {
   CheckIcon,
   ChevronDownIcon,
   ChevronUpIcon,
 } from "@radix-ui/react-icons";
-import { forwardRef, Fragment, ReactNode, Ref } from "react";
+import { forwardRef, ReactNode, Ref } from "react";
 import classNames from "../../lib/utils/classNames";
 import { Label } from "@radix-ui/react-label";
 import Button from "./Button";
@@ -17,37 +18,39 @@ export const Select = forwardRef(function SelectWithRef(
   { children, label, ...props }: SelectProps,
   forwardedRef: Ref<HTMLButtonElement>
 ) {
-  const Wrapper = label ? Label : Fragment;
+  const Wrapper = label ? Label : Slot;
   return (
-    <Wrapper>
-      {label}
-      <SelectPrimitive.Root {...props}>
-        <SelectPrimitive.Trigger
-          asChild
-          className="flex w-full items-center justify-between rounded-lg py-2 px-1 transition duration-300 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
-          ref={forwardedRef}
-        >
-          <Button size="sm">
-            <SelectPrimitive.Value asChild>
-              <span className="w-full truncate">{props.value}</span>
-            </SelectPrimitive.Value>
-            <SelectPrimitive.Icon>
+    <Wrapper asChild>
+      <div>
+        {label}
+        <SelectPrimitive.Root {...props}>
+          <SelectPrimitive.Trigger
+            asChild
+            className="flex w-full items-center justify-between rounded-lg py-2 px-1 transition duration-300 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
+            ref={forwardedRef}
+          >
+            <Button size="sm">
+              <SelectPrimitive.Value asChild>
+                <span className="w-full truncate">{props.value}</span>
+              </SelectPrimitive.Value>
+              <SelectPrimitive.Icon>
+                <ChevronDownIcon />
+              </SelectPrimitive.Icon>
+            </Button>
+          </SelectPrimitive.Trigger>
+          <SelectPrimitive.Content className="rounded-lg bg-white p-2 shadow-lg dark:bg-gray-700">
+            <SelectPrimitive.ScrollUpButton className="flex items-center justify-center text-gray-700 dark:text-gray-300">
+              <ChevronUpIcon />
+            </SelectPrimitive.ScrollUpButton>
+            <SelectPrimitive.Viewport className="max-h-96">
+              {children}
+            </SelectPrimitive.Viewport>
+            <SelectPrimitive.ScrollDownButton className="flex items-center justify-center text-gray-700 dark:text-gray-300">
               <ChevronDownIcon />
-            </SelectPrimitive.Icon>
-          </Button>
-        </SelectPrimitive.Trigger>
-        <SelectPrimitive.Content className="rounded-lg bg-white p-2 shadow-lg dark:bg-gray-700">
-          <SelectPrimitive.ScrollUpButton className="flex items-center justify-center text-gray-700 dark:text-gray-300">
-            <ChevronUpIcon />
-          </SelectPrimitive.ScrollUpButton>
-          <SelectPrimitive.Viewport className="max-h-96">
-            {children}
-          </SelectPrimitive.Viewport>
-          <SelectPrimitive.ScrollDownButton className="flex items-center justify-center text-gray-700 dark:text-gray-300">
-            <ChevronDownIcon />
-          </SelectPrimitive.ScrollDownButton>
-        </SelectPrimitive.Content>
-      </SelectPrimitive.Root>
+            </SelectPrimitive.ScrollDownButton>
+          </SelectPrimitive.Content>
+        </SelectPrimitive.Root>
+      </div>
     </Wrapper>
   );
 });
